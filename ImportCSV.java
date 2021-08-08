132841ea0bdf1c9d77e93dc98aa23679658fbbeb
chthomps <67965487+chthomps@users.noreply.github.com>
2021-08-08 13:57:17 -0500

    Add files via upload

---
+++ ImportCSV.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ImportCSV {
+
+	public static void main(String[] args)throws IOException {
+
+	String path = "/Users/thompsonc/Desktop/iris-training-data.csv";
+	try {
+	//read csv into an array named "trainAttributes"
+		Scanner scan= new Scanner (new BufferedReader(new FileReader(path)));
+	
+	
+	double [][] trainAttributes = new double [25][4];   //25x4 attribute data
+		
+	
+	//read each line of csv, parsing from "line" String array into new 2D double array called trainAttributes
+	while (scan.hasNextLine()) { // read text file line-by-line 
+		String []line= scan.nextLine().split(",");    //insert commas between elements
+		
+		System.out.println (line);
+		
+	}
+	scan.close();
+
+	} finally {}
+}
+}
